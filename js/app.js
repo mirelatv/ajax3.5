43048ae0f46bc117909b89babb58dc19f80b671f
mirelatv <noelia.mirela@gmail.com>
2018-02-14 00:30:02 -0500

    subiendo codigo de  3.5 ajax xhr

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('search-form');
+const searchField=document.getElementById('search-keyword');
+const responseContainer= document.getElementById('response-container');
+let searchedFordText;
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    responseContainer.innerHTML='';
+    searchedFordText=searchField.value;
+    getNews();
+
+})
+
+function getNews(){
+    const articleRequest = new XMLHttpRequest();
+    articleRequest.open('GET',`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=$(searchForText)&api-Key=<2c03c764d6cc430496402090c67d443b>`);
+    articleRequest.onload=addNews;
+    articleRequest.onerror=handlelError;
+    articleRequest.send();
+}
+function handlelError(){
+    console.log('se ha producido unerror');
+}
+function addNews(){
+ const data = JSON.parse(this.responseText);
+ console.log(data);
+ const article = data.response.docs[0];
+ const title= article.headline.main;
+ const snipped= article.snipped;
+ let li = document.createElement('li');
+ li.className='articleClass';
+ li.innerText = snipped;
+ responseContainer.appendChild('li')
+}
+
